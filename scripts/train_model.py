5d5d3d6ebb7a591acffc00a6730a348a461f2806
Matthew Stanton <mstanton@nerdery.com>
2024-11-09 17:11:06 -0600

    update removing formatting errors etc.

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,15 +1,15 @@
    # scripts/train_model.py
-   import pandas as pd
-   from models.neural_network import create_model
+import pandas as pd
+from models.neural_network import create_model
 
-   # Load data
-   data = pd.read_csv('../data/fuel_data.csv')
-   X = data.drop('fuel_efficiency', axis=1).values  # Features
-   y = data['fuel_efficiency'].values  # Target variable
+# Load data
+data = pd.read_csv('../data/fuel_data.csv')
+X = data.drop('fuel_efficiency', axis=1).values  # Features
+y = data['fuel_efficiency'].values  # Target variable
 
-   # Create and train model
-   model = create_model((X.shape[1],))
-   model.fit(X, y, epochs=100, batch_size=32, validation_split=0.2)
+# Create and train model
+model = create_model((X.shape[1],))
+model.fit(X, y, epochs=100, batch_size=32, validation_split=0.2)
 
-   # Save the model
-   model.save('fuel_efficiency_model.h5')
+# Save the model
+model.save('fuel_efficiency_model.h5')
